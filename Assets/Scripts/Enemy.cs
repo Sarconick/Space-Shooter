c13265c9253d077a3fdf079eed858ff332c815b2
Sarconick <nmalketov@gmail.com>
2019-09-16 00:03:19 +0300

    Makin enemy movement more complex and cleaning up code

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,12 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float _enemySpeed = 4.0f;
+    [SerializeField]
+    private float movementDuration = 2.0f;
+    [SerializeField]
+    private float waitBeforeMoving = 2.0f;
+    [SerializeField]
+    private bool hasArrived = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +22,18 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float randX = Random.Range(-9.0f, 9.0f);
+        float randY = Random.Range(-1.5f, 1.5f);
         transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
+        if (!hasArrived)
+        {
+            hasArrived = true;
+            StartCoroutine(MoveToPoint(new Vector3(randX, randY, 0)));
+        }
+
         if (transform.position.y < -7.0f)
         {
-            float randomX = Random.Range(-9.7f, 9.7f);
-            transform.position = new Vector3(randomX, 8, 0);
+            transform.position = new Vector3(randX, 8, 0);
         }
     }
 
@@ -41,5 +54,24 @@ public class Enemy : MonoBehaviour
             Destroy(CollisionObject.gameObject);
             Destroy(this.gameObject);
         }
+    }
+
+    private IEnumerator MoveToPoint(Vector3 TargetPos)
+    {
+        float timer = 0.0f;
+        Vector3 startPos = transform.position;
+
+        while (timer < movementDuration)
+        {
+            timer += Time.deltaTime;
+            float t = timer / movementDuration;
+            t = t * t * t * (t * (6f * t - 15f) + 10f);
+            transform.position = Vector3.Lerp(startPos, TargetPos, t);
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(waitBeforeMoving);
+        hasArrived = false;
     }
 }
--- Assets/Scripts/PowerUps.cs
+++ Assets/Scripts/PowerUps.cs
@@ -7,12 +7,6 @@ public class PowerUps : MonoBehaviour
     [SerializeField]
     private float _speed = 3;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
