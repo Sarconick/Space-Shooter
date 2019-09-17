1a6d6e07c7120a77e2092f4686728a4eee631974
Sarconick <nmalketov@gmail.com>
2019-09-18 00:39:24 +0300

    added UI elements

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,10 +7,17 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]
     private Text _scoreText;
+    [SerializeField]
+    private Image _livesImage;
+    [SerializeField]
+    private Sprite[] _liveSprites;
+    [SerializeField]
+    private Text _gameOverText;
     // Start is called before the first frame update
     void Start()
     {
-        _scoreText.text = "Score: " + 0; 
+        _scoreText.text = "Score: " + 0;
+        _gameOverText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,4 +29,26 @@ public class UIManager : MonoBehaviour
     {
         _scoreText.text = "Score: " + playerScore;
     }
+
+    public void UpdateLives(int currentLives)
+    {
+        _livesImage.sprite = _liveSprites[currentLives];
+        if (currentLives == 0)
+        {
+            _gameOverText.gameObject.SetActive(true);
+            StartCoroutine(GameOverFlicker());
+        }
+    }
+
+    IEnumerator GameOverFlicker()
+    {
+        while (true)
+        {
+            _gameOverText.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
 }
+
